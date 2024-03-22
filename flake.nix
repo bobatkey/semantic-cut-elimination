5c1eb74c202129d53c4604e7b30e942f5544de14
Robert Atkey <robert.atkey@strath.ac.uk>
2024-03-22 14:59:34 +0000

    [ nix ] flake fiddling

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Formalisation of the Vehicle Language in Agda";
+  description = "Semantic Cut Elimination mechanised proofs and documents";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
     flake-utils.url = github:numtide/flake-utils;
@@ -9,6 +9,25 @@
     eachSystem allSystems (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-basic
+            latexmk cmll mathpartir xypic todonotes mathtools xifthen ifmtarg polytable lazylist etoolbox environ
+            newunicodechar
+            # latex-bin latexmk
+            # etoolbox xcolor geometry stmaryrd
+            # mathpartir hyperref pdftexcmds infwarerr
+            # kvoptions cmll ntheorem mathtools
+            # latex-tools-dev amsmath
+            # oberdiek
+            # natbib todonotes
+            # titlesec bbold bbold-type1 epstopdf-pkg upquote
+            # xypic xifthen ifmtarg polytable lazylist environ newunicodechar
+            # metafont ec
+          ;
+        };
+
         agda = pkgs.agda.withPackages
           (ps: [ (ps.standard-library.overrideAttrs (oldAttrs: {
             version = "2.0";
@@ -35,7 +54,31 @@ mkdir -p $out;
 cp html/* $out/;
 '';
           };
+          paper = pkgs.stdenvNoCC.mkDerivation rec {
+            name = "Paper on Semantic Cut Elimination for Multiplicative Additive System Virtual";
+            src = self;
+            buildInputs = [ pkgs.coreutils tex agda pkgs.gnumake pkgs.which pkgs.bash ];
+            phases = ["unpackPhase" "buildPhase" "installPhase"];
+            buildPhase = ''
+export PATH="${pkgs.lib.makeBinPath buildInputs}";
+mkdir -p .cache/texmf-var;
+cd doc/
+env TEXMFHOME=../.cache TEXMFVAR=../.cache/texmf-var make AGDA=agda
+'';
+            installPhase = ''
+mkdir -p $out;
+cp paper.pdf $out/
+'';
+          };
         };
         defaultPackage = packages.html-doc;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues packages;
+          # buildInputs = devPackages ++ [
+          #   pkgs.rsync
+          # ];
+        };
+
       });
 }
